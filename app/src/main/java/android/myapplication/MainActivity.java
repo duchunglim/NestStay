e929fa7b328365ef28a8107d4dca1f989adf1761
DucHungpl <duchunglim@gmail.com>
2024-06-17 18:39:42 +0700

    Done Menu Nav

--- app/src/main/java/android/myapplication/MainActivity.java
+++ app/src/main/java/android/myapplication/MainActivity.java
@@ -1,14 +1,55 @@
 package android.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+        // Set default fragment
+        if (savedInstanceState == null) {
+            loadFragment(new HomeFragment());
+            bottomNavigationView.setSelectedItemId(R.id.nav_home);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment selectedFragment = null;
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            selectedFragment = new HomeFragment();
+        } else if (id == R.id.nav_orders) {
+            selectedFragment = new OrdersFragment();
+        } else if (id == R.id.nav_reservation) {
+            selectedFragment = new ReservationFragment();
+        } else if (id == R.id.nav_profile) {
+            selectedFragment = new ProfileFragment();
+        }
+
+        if (selectedFragment != null) {
+            loadFragment(selectedFragment);
+        }
+
+        return true;
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.commit();
     }
 }
--- app/src/main/java/android/myapplication/OrderCategories.java
+++ app/src/main/java/android/myapplication/OrderCategories.java
@@ -25,6 +25,6 @@ public class OrderCategories extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_order_categories, container, false);
+        return inflater.inflate(R.layout.fragment_orders, container, false);
     }
 }
