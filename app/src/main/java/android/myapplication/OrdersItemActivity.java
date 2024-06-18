a015a9c6986e7c5862b79382d269b86931be4cf4
VongThienLong <vongthienlong12@gmail.com>
2024-06-18 13:03:35 +0700

    update

---
+++ app/src/main/java/android/myapplication/OrdersItemActivity.java
@@ -0,0 +1,14 @@
+package android.myapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OrdersItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
